b6f0e8f44c2136ceedd8a16cb690e4c239fa92d2
Raspac <75386339+Raspac@users.noreply.github.com>
2022-10-01 17:22:16 +0200

    Commit of the little 8 of the Lab1

---
+++ lab00/src/test/java/agh/ii/prinjava/lab00/lst00_01/CalcTest.java
@@ -0,0 +1,12 @@
+package agh.ii.prinjava.lab00.lst00_01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void add() {
+    }
+}
---
+++ proj1/src/test/java/agh/ii/prinjava/proj1/MyStackTest.java
@@ -0,0 +1,7 @@
+package agh.ii.prinjava.proj1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackTest {
+
+}
